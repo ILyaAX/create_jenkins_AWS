248d367b9b03f5e940f049eead2521da872dfdcb
Ilya Aksenov <ilya@aksenov.ws>
2021-11-04 16:45:25 +0700

    provisioner "remote-exec"

--- main.tf
+++ main.tf
@@ -20,12 +20,15 @@ resource "aws_instance" "jenkins" {
   ami = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.jenkins.id]
-  user_data = file("init_jenkins.sh")
   key_name = "awsmykey"
   tags = {
     Name = "jenkins"
   }
 }
+
+provisioner "remote-exec" {
+    inline = file("init_jenkins.sh")
+  }
 
 resource "aws_security_group" "jenkins" {
   name        = "http + ssh traffic"
