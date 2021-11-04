f15996ab4154ec775d989c06f9a76fbad167a52e
Ilya Aksenov <ilya@aksenov.ws>
2021-11-04 15:32:22 +0700

    fix certificates

--- init_jenkins.sh
+++ init_jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update
+sudo apt install ca-certificates
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update && sudo apt upgrade -y
