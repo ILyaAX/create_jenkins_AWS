7577acbf76fa4e9ac01f0f607eb1420eb3a4773d
Ilya Aksenov <ilya@aksenov.ws>
2021-11-04 15:17:37 +0700

    sudo in file .sh

--- init_jenkins.sh
+++ init_jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add
-sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt update && apt upgrade -y
-apt install openjdk-11-jdk -y
-apt install jenkins -y
+sudo apt update
+sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update && sudo apt upgrade -y
+sudo apt install openjdk-11-jdk -y
+sudo apt install jenkins -y
